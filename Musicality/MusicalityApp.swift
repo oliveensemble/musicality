d2cb50008b3dca32f104399b2d6a49ecf3ecf773
Elle Lewis <oliveensemble+dev@gmail.com>
2024-03-07 23:50:34 -0500

    Create App.swift

---
+++ Musicality/MusicalityApp.swift
@@ -0,0 +1,34 @@
+//
+//  MusicalityApp.swift
+//  Musicality
+//
+//  Created by Elle Lewis on 3/7/24.
+//  Copyright © 2024 Evan Lewis. All rights reserved.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MusicalityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
+
