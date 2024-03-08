712450089ac46436e7ce321c0cce74e5e78333fa
Elle Lewis <oliveensemble+dev@gmail.com>
2024-03-08 01:58:17 -0500

    Create tabs

--- Musicality/ContentView.swift
+++ Musicality/ContentView.swift
@@ -5,52 +5,27 @@
 //  Created by Elle Lewis on 3/7/24.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+
+    @State private var selection = 0
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        TabView(selection: $selection) {
+            Text("First Tab")
+                .tabItem {
+                    Label("Explore", image: selection == 0 ? ImageResource.exploreSelectedIcon : ImageResource.exploreIcon)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+                .tag(0)
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            Text("Second Tab")
+                .tabItem {
+                    Label("Artists", image: selection == 1 ? ImageResource.micSelectedIcon : ImageResource.micIcon)
+                }
+                .tag(1)
         }
     }
 }
